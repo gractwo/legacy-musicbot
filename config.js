43a56b28dc9854641d506c677e7996d7d82faa78
Stanislaw <62724833+BoberITman@users.noreply.github.com>
2022-06-26 19:51:23 +0200

    prefix hotfix

--- config.js
+++ config.js
@@ -1,23 +1,35 @@
 module.exports = {
-    app: {
-        px: 'XXX',
-        token: 'XXX',
-        playing: 'by Zerio ❤️'
-    },
+  app: {
+    px: "XXX",
+    token: "XXX",
+    playing: "ciepłe dranie",
+  },
 
-    opt: {
-        DJ: {
-            enabled: false,
-            roleName: 'DJ',
-            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
-        },
-        maxVol: 100,
-        loopMessage: false,
-        discordPlayer: {
-            ytdlOptions: {
-                quality: 'highestaudio',
-                highWaterMark: 1 << 25
-            }
-        }
-    }
+  opt: {
+    DJ: {
+      enabled: false,
+      roleName: "DJ",
+      commands: [
+        "back",
+        "clear",
+        "filter",
+        "loop",
+        "pause",
+        "resume",
+        "seek",
+        "shuffle",
+        "skip",
+        "stop",
+        "volume",
+      ],
+    },
+    maxVol: 100,
+    loopMessage: false,
+    discordPlayer: {
+      ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25,
+      },
+    },
+  },
 };
